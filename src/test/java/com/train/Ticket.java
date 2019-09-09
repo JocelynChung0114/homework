c722ffc694daf368a009b5f6229178bc87b26c41
Jocelyn <jocelyn.chung@toplogis.com>
2019-09-09 17:13:04 +0800

    Initial commit

---
+++ src/test/java/com/train/Ticket.java
@@ -0,0 +1,21 @@
+package com.train;
+
+class Ticket {
+    private int count;
+    private int roundTripCount;
+
+    Ticket(int count, int roundTripCount) {
+        this.count = count;
+        this.roundTripCount = roundTripCount;
+    }
+
+    void print(){
+        System.out.println("Total tickets: "  + count + "\t" +
+                "Round-trip: " + roundTripCount + "\t" +
+                "Total: " + getMoney());
+    }
+
+    private double getMoney(){
+        return (count - roundTripCount) * 1000 + (roundTripCount * 2000 * 0.9);
+    }
+}
---
+++ src/test/kotlin/com/train/TesterKotlin.kt
@@ -0,0 +1,23 @@
+package com.train
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Please enter number of tickets:")
+    val count = scanner.nextInt()
+    println("How many round-trip tickets: ")
+    val roundTripCount = scanner.nextInt()
+    TicketPrice(count, roundTripCount).print()
+
+}
+
+class TicketPrice (var count: Int, var roundTripCount: Int ) {
+    fun print(){
+        print("Total tickets: $count Round-trip: $roundTripCount Total: ${total()}")
+    }
+
+    private fun total (): Double {
+        return  (count - roundTripCount) * 1000 + (roundTripCount * 2000 * 0.9);
+    }
+}
